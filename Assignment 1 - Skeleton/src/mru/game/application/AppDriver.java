f323bee2ed5a516b99b1a55a7da7ec9843aee8a0
jonah <jonah@10.0.0.7>
2022-02-03 18:54:58 -0700

    Initial commit of the Assignment 1 skeleton

--- Assignment 1 - Skeleton/src/mru/game/application/AppDriver.java
+++ Assignment 1 - Skeleton/src/mru/game/application/AppDriver.java
@@ -8,6 +8,8 @@ public class AppDriver {
 		// This is the starting point of the project!
 		// hint - It usually calls method from GameManager class to start the app, so only one or two lines will be written here
 
+		//this line is so that i can commit the changes and push to github
+		System.out.print("test");
 	}
 
 }
